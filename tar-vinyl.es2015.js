c22261084cdab8f135b13630a77c6de780ed0b8b
Thomas Heck <t@b128.net>
2016-03-19 15:40:03 +0100

    + pack function

--- tar-vinyl.es2015.js
+++ tar-vinyl.es2015.js
@@ -1,8 +1,7 @@
 import duplexer from 'duplexer2';
-import {extract as TarExtract} from 'tar-stream';
-import {Readable} from 'readable-stream';
+import {extract as TarExtract, pack as TarPack} from 'tar-stream';
+import {Readable, Writable} from 'readable-stream';
 import Vinyl from 'vinyl';
-
 
 export class Extract extends Readable {
 	constructor(tarExt) {
@@ -31,4 +30,28 @@ export class Extract extends Readable {
 export function extract() {
 	const tarExt = new TarExtract();
 	return duplexer({readableObjectMode: true}, tarExt, new Extract(tarExt));
+}
+
+export class Pack extends Writable {
+	constructor(tarPak) {
+		super({objectMode: true});
+
+		this._tarPak = tarPak;
+
+		this.once('finish', () => this._tarPak.finalize());
+	}
+
+	_write(v, _, cb) {
+		const bufs = [];
+		v.contents.on('data', b => bufs.push(b));
+		v.contents.once('end', () => {
+			const header = Object.assign({}, v.tarHeader || {}, {name: v.path});
+			this._tarPak.entry(header, Buffer.concat(bufs), cb);
+		});
+	}
+}
+
+export function pack() {
+	const tarPak = new TarPack();
+	return duplexer({writableObjectMode: true}, new Pack(tarPak), tarPak);
 }
