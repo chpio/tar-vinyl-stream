160480431b8810ea62f251f31fbdcc5ffbbfda9e
Thomas Heck <t@b128.net>
2016-03-19 15:18:34 +0100

    + debugging to example/extract

--- examples/extract.js
+++ examples/extract.js
@@ -2,9 +2,11 @@ const extract = require('../tar-vinyl').extract;
 const fs = require('fs');
 const gulp = require('gulp');
 const filter = require('through2-filter');
+const debug = require('gulp-debug');
 
 fs.createReadStream(`${__dirname}/test.tar`)
 	.pipe(extract())
+	.pipe(debug())
 	// allow just files & directories
 	.pipe(filter.obj(f => ['file', 'directory'].indexOf(f.tarHeader.type) !== -1))
 	.pipe(gulp.dest('./dest'));
