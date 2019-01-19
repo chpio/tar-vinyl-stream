04bd08fb174eef26ec204925138ebe17ab27dca8
Cody Taylor <codemister99@yahoo.com>
2019-01-18 23:27:00 -0500

    Rewrite to feel like a vinyl adapter.
    * When extracting create a vinyl.stat with proper mode value.
    * When packing use vinyl.stat if available.
    * Handle symbolic links.
    * Add extracting options.
      * buffer - create files with buffer'd ".contents" (default: true)
      * cwd - the vinyl object's ".cwd" (default: `process.cwd()`)
      * base - the vinyl object's ".base" (default: `process.cwd()`)
    * Add packing options.
      * fmode - file mode to use if vinyl.stat.mode is missing
      * dmode - directory mode to use if vinyl.stat.mode is missing
      * umask - mask for dmode & fmode when applicable (default: `process.umask()`)
      * uid - User ID to use if vinyl.stat.uid is missing (default: `process.uid()`)
      * gid - Group ID to use if vinyl.stat.gid is missing (default: `process.gid()`)
      * mtime - file modification time if vinyl.stat.mtime is missing (default: `new Date()`)
    * Always buffer the contents when packing. Only way to guarentee size.
    
    :heavy_plus_sign: bl@2.1.2
    :heavy_plus_sign: stats-ctor@1.1.0
    :heavy_plus_sign: stat-mode@0.2.2

--- tar-vinyl-stream.js
+++ tar-vinyl-stream.js
@@ -1,34 +1,76 @@
+const BufferList = require('bl');
 const Duplexify = require('duplexify');
+const Mode = requrie('stat-mode');
+const Stats = require('stats-ctor');
 const TarExtract = require('tar-stream/extract');
 const TarPack = require('tar-stream/pack');
-const {Readable, Writable} = require('readable-stream');
+const {PassThrough, Readable, Writable} = require('readable-stream');
 const Vinyl = require('vinyl');
+const path = require('path');
 
 class Extract extends Readable {
-	constructor(tarExtract) {
+	constructor(tarExtract, options) {
 		super({objectMode: true});
 
-		// streaming is not supported by tar-stream
-		// https://github.com/mafintosh/tar-stream/issues/50
-		tarExtract.on('entry', (header, contents, next) => {
-			const bufs = [];
+		const cwd = options && options.cwd || process.cwd();
+		const base = options && options.base || cwd;
+		const buffer = !options || options.buffer !== false;
 
-			contents
-				.on('data', b => bufs.push(b))
-				.once('end', () => {
-					const file = new Vinyl({
-						path: header.name,
-						contents: Buffer.concat(bufs),
-					});
+		tarExtract.on('entry', (header, stream, callback) => {
+			const file = new Vinyl({
+				cwd: cwd,
+				base: base,
+				path: path.join(base, header.name),
+				stat: new Stats(header)
+			});
+			const mode = new Mode(file.stat);
+			const next = (err, contents) => {
+				// errors are delivered back to the writable `tarExtract` stream,
+				// where it is emitted, caught by deplexify, and destroys both streams
+				if (err) {
+					return callback(err);
+				}
 
-					file.tarHeader = header;
+				if (contents) {
+					file.contents = contents;
+				}
 
-					if (this.push(file)) {
-						next();
-					} else {
-						this.once('drain', next);
-					}
-				});
+				this.push(file);
+				callback(null);
+			};
+			const ignore = onend => stream.once('end', onend).resume();
+
+			switch (header.type) {
+			case 'file':
+				mode.isFile(true);
+
+				if (buffer) {
+					stream.pipe(new BufferList(next));
+				} else {
+					const contents = new PassThrough();
+
+					stream
+						.once('error', next)
+						.once('end', () => next(null, contents))
+						.pipe(contents);
+				}
+				break;
+			case 'directory':
+				mode.isDirectory(true);
+				ignore(next);
+				break;
+			case 'symlink':
+				mode.isSymbolicLink(true);
+				file.symlink = header.linkname;
+				ignore(next);
+				break;
+			default:
+				const err = new TypeError(`tar-vinyl-stream: unable to handle type "${header.type}" for "${header.name}"`);
+
+				err.code = 'ETARTYPE';
+				ignore(() => next(err));
+				break;
+			}
 		});
 
 		tarExtract.once('finish', () => this.push(null));
@@ -39,40 +81,87 @@ class Extract extends Readable {
 	}
 }
 
-function extract() {
+function extract(options) {
 	const tarExtract = new TarExtract();
 
-	return new Duplexify(tarExtract, new Extract(tarExtract), {
+	return new Duplexify(tarExtract, new Extract(tarExtract, options), {
 		readableObjectMode: true
 	});
 }
 
 class Pack extends Writable {
-	constructor(tarPack) {
+	constructor(tarPack, options) {
 		super({objectMode: true});
 
-		this._tarPak = tarPack;
+		options = options || {};
 
-		this.once('finish', () => this._tarPak.finalize());
+		this._tarPack = tarPack;
+		this._umask = this._options.umask || process.umask();
+		this._fileMode = this._options.fmode || 0o666;
+		this._dirMode = this._options.dmode || 0o777;
+		this._uid = this._options.uid || process.uid();
+		this._gid = this._options.gid || process.gid();
+		this._mtime = this._options.mtime || new Date();
 	}
 
 	_write(file, _, next) {
-		const header = Object.assign({}, file.tarHeader || {}, {name: file.relative});
+		const stat = file.stat;
+		const header = {
+			name: file.relative,
+			type: 'file',
+			mode: stat.mode,
+			uid: stat.uid || this._uid,
+			gid: stat.gid || this._gid,
+			mtime: stat.mtime || this._mtime
+		};
+		const onfile = (err, buffer) => {
+			if (err) return next(err);
+
+			if (!header.mode) {
+				header.mode = this._fileMode ^ this._umask;
+			}
+
+			header.size = buffer ? buffer.length : 0;
+
+			this._tarPack.entry(header, buffer, next);
+		};
 
 		if (file.isBuffer()) {
-			this._tarPak.entry(header, file.contents, next);
+			onfile(null, file.contents);
 		} else if (file.isStream()) {
-			file.contents.pipe(this._tarPak.entry(header)).once('end', next);
+			// buffer the stream because the byte count must be exact, but we
+			// cannot know what transformations were applied to file.contents
+			file.contents.pipe(new BufferList(onfile));
+		} else if (file.isSymbolic()) {
+			header.type = 'symlink';
+			header.linkname = file.symlink;
+			onfile(null, null);
+		} else if (file.isDirectory()) {
+			header.size = 0;
+			header.type = 'directory';
+
+			if (!header.mode) {
+				header.mode = this._dirMode ^ this._umask;
+			}
+
+			this._tarpack(header, next);
 		} else {
-			next(new TypeError(`${file} is not a buffer or stream`));
+			const err = new TypeError(`tar-vinyl-stream.pack: unknown file type for "${header.name}"`);
+
+			err.code = 'ETARTYPE';
+			next(err);
 		}
+	}
+
+	_final(done) {
+		this._tarPack.finalize();
 	}
 }
 
-function pack() {
+function pack(options) {
 	const tarPack = new TarPack();
 
-	return new Duplexify(new Pack(tarPack), tarPack, {
+	return new Duplexify(new Pack(tarPack, options), tarPack, {
 		writableObjectMode: true
 	});
 }
