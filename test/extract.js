1d4b4bf13f603f629e09e515c2474279e016b3fe
Cody Taylor <codemister99@yahoo.com>
2019-02-03 22:11:06 -0500

    Update extract test to use new fixtures.

--- test/extract.js
+++ test/extract.js
@@ -1,62 +1,28 @@
 import fs from 'fs';
 import path from 'path';
-import {promisify} from 'util';
-import {spawn} from 'child_process';
-import BufferList from 'bl';
+import util from 'util';
 import {extract} from '..';
 import getStream from 'get-stream';
 import {pipeline} from 'readable-stream';
 import test from 'ava';
 
-const bufferFile = promisify(fs.readFile);
-const pump = promisify(pipeline);
-
-function tarCommand(...filenames) {
-	const tar = spawn('tar', ['-c', '--', ...filenames], {
-		cwd: __dirname
-	});
-	const stream = tar.stdout;
-
-	tar.stderr.pipe(new BufferList((err, stderr) => {
-		const data = stderr ? stderr.toString().trim() : null;
-
-		err = err || (data && new Error(data));
-
-		if (err) {
-			stream.destroy(err);
-		}
-	}));
-
-	tar.once('exit', (exitCode, signal) => {
-		stream.exitCode = exitCode;
-
-		if (exitCode > 0) {
-			const err = new Error(`tar exitCode=${exitCode} signal=${signal}`);
-
-			stream.destroy(err);
-		}
-	});
-
-	stream.on('addListener', (event, listener) => tar.on(event, listener));
-	stream.on('removeListener', (event, listener) => tar.removeListener(event, listener));
-
-	return stream;
-}
+const fixtures = path.join(__dirname, 'fixtures');
+const pump = util.promisify(pipeline);
+const readFile = util.promisify(fs.readFile);
 
 test('extract', async t => {
-	const a = 'fixtures/a.txt';
-	const b = 'fixtures/b.txt';
-	const aContents = bufferFile(path.join(__dirname, a));
-	const bContents = bufferFile(path.join(__dirname, b));
-	const e = extract({
-		cwd: __dirname,
-		base: __dirname
-	});
+	const src = fs.createReadStream(path.join(fixtures, 'tarballs', 'files-bare.tar'));
+	const e = extract();
 
-	await pump(tarCommand(a, b), e);
+	await pump(src, e);
 
-	const [files, aBuffer, bBuffer] = await Promise.all([getStream.array(e), aContents, bContents]);
+	const files = await getStream.array(e);
+	const contents = await Promise.all(files.map(file => readFile(path.join(fixtures, 'files', file.basename))));
 
-	t.deepEqual(files[0].contents, aBuffer);
-	t.deepEqual(files[1].contents, bBuffer);
+	for (let i = 0; i < files.length; i++) {
+		const file = files[i];
+		const buffer = contents[i];
+
+		t.deepEqual(file.contents, buffer);
+	}
 });
