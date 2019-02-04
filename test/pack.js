c7c1e503fcc22164c342589d57a0b70b4332a5a3
Cody Taylor <codemister99@yahoo.com>
2019-02-03 23:08:14 -0500

    Update pack test to use new fixtures (BROKEN).
    This test is currently broken because tar-stream follows the pattern of
    tar on OS X (bsdtar / libarchive). That implementation uses more whitespace
    in the header line than GNU tar.

--- test/pack.js
+++ test/pack.js
@@ -1,94 +1,25 @@
 import fs from 'fs';
 import path from 'path';
-import {promisify} from 'util';
-import {spawn} from 'child_process';
-import BufferList from 'bl';
-import Duplexify from 'duplexify';
-import File from 'vinyl';
-import {Readable, pipeline} from 'readable-stream';
+import util from 'util';
+import getStream from 'get-stream';
+import {pipeline} from 'readable-stream';
 import {pack} from '..';
 import test from 'ava';
+import vfs from 'vinyl-fs';
 
-fs.readFile = promisify(fs.readFile);
-fs.stat = promisify(fs.stat);
-
-function tarCommand() {
-	// Cannot use tar verbose because it uses stderr
-	const tar = spawn('tar', ['-x', '--to-stdout'], {
-		cwd: __dirname
-	});
-	const stream = new Duplexify(tar.stdin, tar.stdout);
-
-	tar.stderr.pipe(new BufferList((err, stderr) => {
-		const data = stderr ? stderr.toString().trim() : null;
-
-		err = err || (data && new Error(data));
-
-		if (err) {
-			stream.destroy(err);
-		}
-	}));
-
-	tar.once('exit', (exitCode, signal) => {
-		stream.exitCode = exitCode;
-
-		if (exitCode > 0) {
-			const err = new Error(`tar exitCode=${exitCode} signal=${signal}`);
-
-			stream.destroy(err);
-		}
-	});
-
-	stream.on('addListener', (event, listener) => tar.on(event, listener));
-	stream.on('removeListener', (event, listener) => tar.on(event, listener));
-
-	return stream;
-}
+const fixtures = path.join(__dirname, 'fixtures');
+const pump = util.promisify(pipeline);
+const readFile = util.promisify(fs.readFile);
 
 test('pack', async t => {
-	const aFilename = path.join(__dirname, 'fixtures', 'a.txt');
-	const bFilename = path.join(__dirname, 'fixtures', 'b.txt');
-	const src = new Readable({objectMode: true, read() {}});
-	const finish = new Promise((resolve, reject) => {
-		const dest = new BufferList((err, buffer) => {
-			if (err) {
-				reject(err);
-			} else {
-				resolve(buffer);
-			}
-		});
+	const cwd = path.join(fixtures, 'files');
+	const src = vfs.src('{a,b}.txt', {cwd});
+	const p = pack({cwd});
 
-		pipeline(src, pack({cwd: __dirname, base: __dirname}), tarCommand(), dest, err => {
-			if (err) {
-				reject(err);
-			}
-		});
-	});
+	await pump(src, p, fs.createWriteStream('/tmp/pack.tar'));
 
-	const load = [];
+	const contents = await getStream.buffer(p);
+	const buffer = await readFile(path.join(fixtures, 'tarballs', 'files-bare.tar'));
 
-	for (const filename of [aFilename, bFilename]) {
-		load.push(filename, fs.readFile(filename), fs.stat(filename));
-	}
-
-	const loaded = await Promise.all(load);
-
-	for (let i = 0; i < loaded.length; i += 3) {
-		const [filename, contents, stat] = loaded.slice(i, i + 3);
-
-		src.push(new File({
-			cwd: __dirname,
-			base: __dirname,
-			contents,
-			path: filename,
-			stat
-		}));
-	}
-
-	src.push(null);
-
-	const buffer = await finish;
-	const expected = Buffer.from('a content\nb content\n');
-
-	t.deepEqual(buffer, expected);
+	t.deepEqual(contents, buffer);
 });
