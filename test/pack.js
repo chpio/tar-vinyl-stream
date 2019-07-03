589b8d97390c904cdc4fb6e7dcdc2c85cce1414c
Cody Taylor <codemister99@yahoo.com>
2019-07-03 10:13:33 -0400

    Compare pack result to tar-fs result.

--- test/pack.js
+++ test/pack.js
@@ -1,25 +1,36 @@
-import fs from 'fs';
 import path from 'path';
-import util from 'util';
 import getStream from 'get-stream';
-import {pipeline} from 'readable-stream';
 import {pack} from '..';
+import {pipeline} from 'readable-stream';
+import tarFs from 'tar-fs';
 import test from 'ava';
 import vfs from 'vinyl-fs';
 
 const fixtures = path.join(__dirname, 'fixtures');
-const pump = util.promisify(pipeline);
-const readFile = util.promisify(fs.readFile);
 
 test('pack', async t => {
 	const cwd = path.join(fixtures, 'files');
 	const src = vfs.src('{a,b}.txt', {cwd});
-	const p = pack({cwd});
+	const dest = pack({cwd});
 
-	await pump(src, p, fs.createWriteStream('/tmp/pack.tar'));
+	pipeline(src, dest, err => err && t.fail(err));
 
-	const contents = await getStream.buffer(p);
-	const buffer = await readFile(path.join(fixtures, 'tarballs', 'files-bare.tar'));
+	const contents = await getStream.buffer(dest);
+	const correct = await getStream.buffer(tarFs.pack(cwd));
 
-	t.deepEqual(contents, buffer);
+	for (const chunk of compareTarballChunks(correct, contents)) {
+		t.true(chunk);
+	}
 });
+
+function * compareTarballChunks(correct, testing) {
+	// The correct archive may be longer because it has directories that are not part of the assertion
+	yield testing.length <= correct.length;
+
+	for (let i = 0; i < testing.length; i += 512) {
+		const chunk = testing.slice(i, i + 512);
+
+		// Position of this result does not matter, just that the chunk exists in the correct archive
+		yield correct.includes(chunk);
+	}
+}
